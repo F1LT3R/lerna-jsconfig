11de54bb68bfc4996baa4688747716d0331630b5
Alistair MacDonald <alistair.macdonald@hmhco.com>
2018-11-02 14:36:23 -0400

    First commit.

---
+++ index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+const {execSync} = require('child_process');
+const findUp = require('find-up');
+
+const lernaPkgList = String(execSync('lerna list --json'));
+const pkgListJson = JSON.parse(lernaPkgList);
+
+const lernaFilePath = findUp.sync('lerna.json');
+const lernaRoot = path.parse(lernaFilePath).dir;
+
+const jsConfigPath = path.join(lernaRoot, 'jsconfig.json');
+const jsConfig = require(jsConfigPath);
+
+const newJsConfigPaths = {};
+
+pkgListJson.forEach(package => {
+    newJsConfigPaths[package.name] = [package.location];
+});
+
+jsConfig.paths = newJsConfigPaths;
+
+fs.writeFileSync(jsConfigPath, JSON.stringify(jsConfig, null, 2));
